b948165de5bd0994cdb2599e69c7825f086d1638
Jorge <jorge.quidel312@gmail.com>
2022-10-08 16:56:11 -0300

    agrega funcionalidades a las clases

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -4,42 +4,27 @@ public class Calculadora {
 	private int n2;
 
 	public Calculadora() {
-		// TODO - implement Calculadora.Calculadora
-		throw new UnsupportedOperationException();
+		this.n1 = 0;
+		this.n2 = 0;
 	}
 
-	/**
-	 * 
-	 * @param num1
-	 * @param num2
-	 */
 	public Calculadora(int num1, int num2) {
-		// TODO - implement Calculadora.Calculadora
-		throw new UnsupportedOperationException();
+		this.n1 = num1;
+		this.n2 = num2;
 	}
 
 	public int sumar() {
-		// TODO - implement Calculadora.sumar
-		throw new UnsupportedOperationException();
+		return this.n1+this.n2;
 	}
 
 	public int multiplicar() {
-		// TODO - implement Calculadora.multiplicar
-		throw new UnsupportedOperationException();
+		return this.n1*this.n2;
 	}
 
-	/**
-	 * 
-	 * @param num1
-	 */
 	public void setN1(int num1) {
 		this.n1 = num1;
 	}
 
-	/**
-	 * 
-	 * @param num2
-	 */
 	public void setN2(int num2) {
 		this.n2 = num2;
 	}
